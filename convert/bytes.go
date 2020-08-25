407f2900038728b8b27aff7bf3ff96a328a9a421
Scythefly <fantasticfarm@hotmail.com>
2020-08-25 09:17:31 +0800

    convert/bytes

---
+++ convert/bytes.go
@@ -0,0 +1,21 @@
+package convert
+
+import (
+	"bytes"
+	"io/ioutil"
+
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+)
+
+// DecodeGBK ...
+// convert gbk to utf-8
+func DecodeGBK(s []byte) ([]byte, error) {
+	I := bytes.NewReader(s)
+	O := transform.NewReader(I, simplifiedchinese.GBK.NewDecoder())
+	d, e := ioutil.ReadAll(O)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module pkg
 
 go 1.14
 
-require github.com/valyala/fasthttp v1.16.0
+require (
+	github.com/valyala/fasthttp v1.16.0
+	golang.org/x/text v0.3.0
+)
