11ef67211d45222585479efdb84dd18d393dcd1f
Scythefly <fantasticfarm@hotmail.com>
2023-01-10 13:41:02 +0800

    add local interfaces api

--- net/net.go
+++ net/net.go
@@ -1,31 +1,94 @@
 package net
 
 import (
-	"net"
+    "fmt"
+    "net"
+
+    "github.com/kakami/pkg/types"
 )
 
 func CurrentIP() ([]string, error) {
-	var out []string
-	var err error
+    var out []string
+    var err error
 
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		return out, err
-	}
-	for _, address := range addrs {
-		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				out = append(out, ipnet.IP.String())
-			}
-		}
-	}
-	return out, err
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+        return out, err
+    }
+    for _, address := range addrs {
+        if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+            if ipnet.IP.To4() != nil {
+                out = append(out, ipnet.IP.String())
+            }
+        }
+    }
+    return out, err
 }
 
 func DefaultIP() (string, error) {
-	return getDefaultIP()
+    return getDefaultIP()
 }
 
 func SetProxy(addr, port string, enable bool) {
-	setSystemProxy(addr, port, enable)
+    setSystemProxy(addr, port, enable)
+}
+
+func LocalInterfaces() ([]string, error) {
+    iis, err := net.Interfaces()
+    if err != nil {
+        return nil, err
+    }
+    ips := types.NewUnsafeSet()
+    for idx := range iis {
+        if iis[idx].Flags&net.FlagUp == 0 {
+            continue
+        }
+        if iis[idx].Flags&net.FlagLoopback != 0 {
+            continue
+        }
+        addrs, err := iis[idx].Addrs()
+        if err != nil {
+            return nil, err
+        }
+
+        for i := range addrs {
+            ip := getIpFromAddr(addrs[i])
+            if ip == nil {
+                continue
+            }
+            _, ipn, err := net.ParseCIDR(addrs[i].String())
+            if err != nil {
+                return nil, err
+            }
+            m, _ := ipn.Mask.Size()
+            if m == 32 {
+                continue
+            }
+            ips.Add(ip.String())
+        }
+    }
+
+    if ips.Length() < 1 {
+        return nil, fmt.Errorf("no valid interface")
+    }
+
+    return ips.Values(), nil
+}
+
+func getIpFromAddr(addr net.Addr) net.IP {
+    var ip net.IP
+    switch v := addr.(type) {
+    case *net.IPNet:
+        ip = v.IP
+    case *net.IPAddr:
+        ip = v.IP
+    }
+    if ip == nil || ip.IsLoopback() {
+        return nil
+    }
+    ip = ip.To4()
+    if ip == nil {
+        return nil // not an ipv4 address
+    }
+    return ip
 }
--- net/net_test.go
+++ net/net_test.go
@@ -1,19 +1,23 @@
 package net_test
 
 import (
-	"fmt"
-	"testing"
+    "fmt"
+    "testing"
 
-	"github.com/kakami/pkg/net"
+    "github.com/kakami/pkg/net"
 )
 
 func Test_CurrentIP(t *testing.T) {
-	ips, err := net.CurrentIP()
-	fmt.Println(ips)
-	fmt.Println(err)
+    ips, err := net.CurrentIP()
+    fmt.Println(ips)
+    fmt.Println(err)
 }
 
 func Test_Region(t *testing.T) {
-	t.Log(net.RegionCode("45.32.65.1"))
-	t.Log(net.RegionCode("8.8.8.8"))
+    t.Log(net.RegionCode("45.32.65.1"))
+    t.Log(net.RegionCode("8.8.8.8"))
+}
+
+func Test_LocalInterfaces(t *testing.T) {
+    t.Log(net.LocalInterfaces())
 }
