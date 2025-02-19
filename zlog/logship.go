38ff100bcb9b4a2a4c255ef8acf7fa0e9545ade2
scythefly <fantasticfarm@hotmail.com>
2025-02-19 15:50:51 +0800

    simple log

--- zlog/logship.go
+++ zlog/logship.go
@@ -1,82 +1,93 @@
 package zlog
 
 import (
-    "io"
-    "os"
-    "sync"
+	"io"
+	"os"
+	"sync"
 
-    "go.uber.org/zap"
-    "go.uber.org/zap/zapcore"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var (
-    _defaultLogship = New(os.Stdout, zapcore.InfoLevel)
+	_defaultLogship = New(os.Stdout, zapcore.InfoLevel)
 )
 
 func Default() *Logship {
-    return _defaultLogship
+	return _defaultLogship
 }
 
 func SetWriter(w io.Writer) {
-    _defaultLogship.SetWriter(w)
+	_defaultLogship.SetWriter(w)
 }
 
 func SetDefaultLevel(l zapcore.Level) {
-    _defaultLogship.SetDefaultLevel(l)
+	_defaultLogship.SetDefaultLevel(l)
 }
 
 func LoggerWithTag(tag string) *zap.Logger {
-    return _defaultLogship.LoggerWithTag(tag)
+	return _defaultLogship.LoggerWithTag(tag)
+}
+
+func SimpleLoggerWithTag(tag string) *zap.Logger {
+	return _defaultLogship.SimpleLoggerWithTag(tag)
 }
 
 func SetLevel(tag string, l zapcore.Level) {
-    _defaultLogship.SetLevel(tag, l)
+	_defaultLogship.SetLevel(tag, l)
 }
 
 func Enabled(tag string, l zapcore.Level) bool {
-    return _defaultLogship.Enabled(tag, l)
+	return _defaultLogship.Enabled(tag, l)
 }
 
 type Logship struct {
-    w     io.Writer
-    lvls  sync.Map
-    level zapcore.Level
+	w     io.Writer
+	lvls  sync.Map
+	level zapcore.Level
 }
 
 func New(w io.Writer, level zapcore.Level) *Logship {
-    return &Logship{
-        w:     w,
-        level: level,
-    }
+	return &Logship{
+		w:     w,
+		level: level,
+	}
 }
 
 func (ls *Logship) SetWriter(w io.Writer) {
-    ls.w = w
+	ls.w = w
 }
 
 func (ls *Logship) SetDefaultLevel(l zapcore.Level) {
-    ls.level = l
+	ls.level = l
 }
 
 func (ls *Logship) LoggerWithTag(tag string) *zap.Logger {
-    ptag := "[" + tag + "]"
-    lvli, _ := ls.lvls.LoadOrStore(tag, zap.NewAtomicLevelAt(ls.level))
-    lvl, _ := lvli.(zap.AtomicLevel)
-    return DefaultLoggerWithLevel(ls.w, lvl).Named(ptag)
+	ptag := "[" + tag + "]"
+	lvli, _ := ls.lvls.LoadOrStore(tag, zap.NewAtomicLevelAt(ls.level))
+	lvl, _ := lvli.(zap.AtomicLevel)
+	return DefaultLoggerWithLevel(ls.w, lvl).Named(ptag)
+}
+
+func (ls *Logship) SimpleLoggerWithTag(tag string) *zap.Logger {
+	ptag := "[" + tag + "]"
+	lvli, _ := ls.lvls.LoadOrStore(tag, zap.NewAtomicLevelAt(ls.level))
+	lvl, _ := lvli.(zap.AtomicLevel)
+	return SimpleLoggerWithLevel(ls.w, lvl).Named(ptag)
 }
 
 func (ls *Logship) SetLevel(tag string, l zapcore.Level) {
-    lvli, _ := ls.lvls.LoadOrStore(tag, zap.NewAtomicLevel())
-    lvl, _ := lvli.(zap.AtomicLevel)
-    lvl.SetLevel(l)
+	lvli, _ := ls.lvls.LoadOrStore(tag, zap.NewAtomicLevel())
+	lvl, _ := lvli.(zap.AtomicLevel)
+	lvl.SetLevel(l)
 }
 
 func (ls *Logship) Enabled(tag string, l zapcore.Level) bool {
-    lvli, ok := ls.lvls.Load(tag)
-    if ok {
-        if lvl, ok := lvli.(zap.AtomicLevel); ok {
-            return lvl.Enabled(l)
-        }
-    }
-    return false
+	lvli, ok := ls.lvls.Load(tag)
+	if ok {
+		if lvl, ok := lvli.(zap.AtomicLevel); ok {
+			return lvl.Enabled(l)
+		}
+	}
+	return false
 }
--- zlog/zlog.go
+++ zlog/zlog.go
@@ -1,93 +1,113 @@
 package zlog
 
 import (
-    "io"
-    "time"
+	"io"
+	"time"
 
-    "go.uber.org/zap"
-    "go.uber.org/zap/zapcore"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var (
-    LogBufferLength = 1024
-    LogWithBlocking = true
+	LogBufferLength = 1024
+	LogWithBlocking = true
 )
 
 // SetLogBufferLength (default is 1024)
 // This should be invoked before create logWriter
 func SetLogBufferLength(bufferLen int) {
-    LogBufferLength = bufferLen
+	LogBufferLength = bufferLen
 }
 
 // SetLogWithBlocking (default is true)
 // This should be invoked before create logWriter
 func SetLogWithBlocking(isBlocking bool) {
-    LogWithBlocking = isBlocking
+	LogWithBlocking = isBlocking
 }
 
 func timeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
-    enc.AppendString(t.Format("2006-01-02 15:04:05"))
+	enc.AppendString(t.Format("2006-01-02 15:04:05"))
 }
 
 func DefaultLogger(w io.Writer) *zap.Logger {
-    encoderConf := &zapcore.EncoderConfig{
-        MessageKey:     "msg",
-        LevelKey:       "level",
-        TimeKey:        "time",
-        NameKey:        "logger",
-        CallerKey:      "caller",
-        StacktraceKey:  "stacktrace",
-        LineEnding:     zapcore.DefaultLineEnding,
-        EncodeLevel:    zapcore.LowercaseLevelEncoder,
-        EncodeTime:     timeEncoder,
-        EncodeDuration: zapcore.SecondsDurationEncoder,
-        EncodeCaller:   zapcore.ShortCallerEncoder,
-        EncodeName:     zapcore.FullNameEncoder,
-    }
-    atomicLevel := zap.NewAtomicLevel()
-    atomicLevel.SetLevel(zapcore.DebugLevel)
-    core := zapcore.NewCore(
-        zapcore.NewConsoleEncoder(*encoderConf),
-        zapcore.AddSync(w),
-        atomicLevel,
-    )
-    log := zap.New(core).WithOptions(zap.AddCaller())
+	encoderConf := &zapcore.EncoderConfig{
+		MessageKey:     "msg",
+		LevelKey:       "level",
+		TimeKey:        "time",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     timeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+		EncodeName:     zapcore.FullNameEncoder,
+	}
+	atomicLevel := zap.NewAtomicLevel()
+	atomicLevel.SetLevel(zapcore.DebugLevel)
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(*encoderConf),
+		zapcore.AddSync(w),
+		atomicLevel,
+	)
+	log := zap.New(core).WithOptions(zap.AddCaller())
 
-    return log
+	return log
 }
 
 func DefaultLoggerWithLevel(w io.Writer, lvl zap.AtomicLevel) *zap.Logger {
-    encoderConf := &zapcore.EncoderConfig{
-        MessageKey:     "msg",
-        LevelKey:       "level",
-        TimeKey:        "time",
-        NameKey:        "logger",
-        CallerKey:      "caller",
-        StacktraceKey:  "stacktrace",
-        LineEnding:     zapcore.DefaultLineEnding,
-        EncodeLevel:    zapcore.LowercaseLevelEncoder,
-        EncodeTime:     timeEncoder,
-        EncodeDuration: zapcore.SecondsDurationEncoder,
-        EncodeCaller:   zapcore.ShortCallerEncoder,
-        EncodeName:     zapcore.FullNameEncoder,
-    }
-    core := zapcore.NewCore(
-        zapcore.NewConsoleEncoder(*encoderConf),
-        zapcore.AddSync(w),
-        lvl,
-    )
-    log := zap.New(core).WithOptions(zap.AddCaller())
+	encoderConf := &zapcore.EncoderConfig{
+		MessageKey:     "msg",
+		LevelKey:       "level",
+		TimeKey:        "time",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     timeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+		EncodeName:     zapcore.FullNameEncoder,
+	}
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(*encoderConf),
+		zapcore.AddSync(w),
+		lvl,
+	)
+	log := zap.New(core).WithOptions(zap.AddCaller())
 
-    return log
+	return log
+}
+
+func SimpleLoggerWithLevel(w io.Writer, lvl zap.AtomicLevel) *zap.Logger {
+	encoderConf := &zapcore.EncoderConfig{
+		MessageKey:     "msg",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     timeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+	}
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(*encoderConf),
+		zapcore.AddSync(w),
+		lvl,
+	)
+	log := zap.New(core).WithOptions(zap.AddCaller(), zap.AddStacktrace(zapcore.FatalLevel))
+
+	return log
 }
 
 func NewLogger(w io.Writer, cfg *zapcore.EncoderConfig, lvl zap.AtomicLevel) *zap.Logger {
-    core := zapcore.NewCore(
-        zapcore.NewConsoleEncoder(*cfg),
-        zapcore.AddSync(w),
-        lvl,
-    )
-    log := zap.New(core).WithOptions(zap.AddCaller())
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(*cfg),
+		zapcore.AddSync(w),
+		lvl,
+	)
+	log := zap.New(core).WithOptions(zap.AddCaller())
 
-    return log
+	return log
 }
